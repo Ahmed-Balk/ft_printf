63b78faeaf42cbe0abda8b1c7bfac99fbe4754f7
Pookie <Pookie@bear.me>
2024-11-26 11:04:02 +0100

    vfinal

--- ft_printf.c
+++ ft_printf.c
@@ -6,30 +6,32 @@
 /*   By: abalk <abalk@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 09:27:15 by abalk             #+#    #+#             */
-/*   Updated: 2024/11/25 23:55:14 by abalk            ###   ########.fr       */
+/*   Updated: 2024/11/26 11:00:47 by abalk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static size_t	conv(const char *c, va_list *ap)
+static size_t	conv(const char c, va_list *ap)
 {
-	if (*c == 'd' || *c == 'i')
+	if (c == 'd' || c == 'i')
 		return (ft_putnbr(va_arg(*ap, const int)));
-	if (*c == 'c')
+	if (c == 'c')
 		return (ft_putchar((char) va_arg(*ap, int)));
-	if (*c == 's')
+	if (c == 's')
 		return (ft_putstr(va_arg(*ap, const char *)));
-	if (*c == 'x')
+	if (c == 'x')
 		return (ft_puthexa_lower(va_arg(*ap, unsigned int)));
-	if (*c == 'X')
+	if (c == 'X')
 		return (ft_puthexa_upper(va_arg(*ap, unsigned int)));
-	if (*c == '%')
+	if (c == '%')
 		return (ft_putchar('%'));
-	if (*c == 'u')
+	if (c == 'u')
 		return (ft_printunsigned(va_arg(*ap, unsigned int)));
-	if (*c == 'p')
+	if (c == 'p')
 		return (ft_putadd(va_arg(*ap, void *)));
+	if (c == '\0')
+		return (-1);
 	else
 	{
 		return (-1);
@@ -39,27 +41,27 @@ static size_t	conv(const char *c, va_list *ap)
 int	ft_printf(const char *str, ...)
 {
 	int		pr_char;
-	int		tmp;
+	int		i;
 	va_list	args;
+	int		tmp;
 
-	if (!str)
-		return (0);
+	i = 0;
 	va_start(args, str);
 	pr_char = 0;
-	while (*str)
+	while (str[i])
 	{
-		if (*str == '%' && *(str + 1))
+		if (str[i] == '%')
+			tmp = conv(str[++i], &args);
+		else
+			tmp = ft_putchar(str[i]);
+		if (tmp < 0)
 		{
-			str++;
-			tmp = conv(str, &args);
+			pr_char = tmp;
+			break ;
 		}
 		else
-			tmp = ft_putchar(*str);
-		if (tmp >= 0)
 			pr_char += tmp;
-		else
-			pr_char = tmp;
-		str++;
+		i++;
 	}
 	va_end(args);
 	return (pr_char);
---
+++ main.c
@@ -0,0 +1,8 @@
+#include "ft_printf.h"
+#include <stdio.h>
+
+int main()
+{
+    printf("%d\n ", ft_printf("ahmed %j"));
+    printf("%d\n ", ft_printf("ahmed % fsfz"));
+}
